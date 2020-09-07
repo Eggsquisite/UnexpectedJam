455668b14de7bd9b78bbf7a8404f461065f89af3
Marc Sevilleja <marc@sevilayha.com>
2020-09-06 21:58:34 -0700

    Character movement script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5f;
+    [Header("Horizontal Movement")]
+    [SerializeField] float moveSpeed = 15f;
+    Vector2 direction;
+    bool facingRight = true;
 
+    [Header("Components")]
     private Rigidbody2D rb;
     private Animator anim;
-    Vector2 direction;
+
+    [Header("Physics")]
+    [SerializeField] float maxSpeed = 7f;
+    [SerializeField] float linearDrag = 4f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,23 +28,41 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), transform.position.y);
-        anim.SetFloat("speed", Mathf.Abs(direction.x));
-
-        if (direction.x > 0)
-            transform.localScale = new Vector3(1, 1, 1);
-        else if (direction.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
-
-        //Movement(horizontal);
     }
 
     private void FixedUpdate()
     {
-        Movement(direction);
+        Movement(direction.x);
+        ModifyPhysics();
     }
 
-    void Movement(Vector2 direction)
+    void Movement(float direction)
+    {   
+        rb.AddForce(Vector2.right * direction * moveSpeed);
+
+
+        if ((direction > 0 && !facingRight) || (direction < 0 && facingRight))
+            Flip();
+
+        if (Mathf.Abs(rb.velocity.x) > maxSpeed)
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+
+        anim.SetFloat("horizontal", Mathf.Abs(rb.velocity.x));
+    }
+
+    void Flip()
     {
-        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+        facingRight = !facingRight;
+        transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180, 0);
+    }
+
+    void ModifyPhysics()
+    {
+        bool changingDirections = (direction.x > 0 && rb.velocity.x < 0) || (direction.x < 0 && rb.velocity.x > 0);
+
+        if (Mathf.Abs(direction.x) < 0.4f || changingDirections)
+            rb.drag = linearDrag;
+        else
+            rb.drag = 0;
     }
 }
