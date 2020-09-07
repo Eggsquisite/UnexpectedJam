6baac619bf52c5c2e4b5085df998ee01c744cb29
Marc Sevilleja <marc@sevilayha.com>
2020-09-06 21:11:14 -0700

    Implemented basic left/right movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (rb == null) rb = GetComponent<Rigidbody2D>();
+        if (anim == null) anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), transform.position.y);
+        anim.SetFloat("speed", Mathf.Abs(direction.x));
+
+        if (direction.x > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (direction.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        //Movement(horizontal);
+    }
+
+    private void FixedUpdate()
+    {
+        Movement(direction);
+    }
+
+    void Movement(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
